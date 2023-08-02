a3d3f9c3013d0aa676e079a4815f88afdb03d0ed
Mengistu Ashebir <95374219+mengesha9@users.noreply.github.com>
2023-08-02 02:02:31 -0700

    Create Rectangle.cs

---
+++ Aug2_2023/Task _1_Building_a_Shape_Hierarchy/Rectangle.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingShape
+{
+    internal class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        
+
+        public double Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public override double calculateArea()
+        {
+            return width * height;
+
+        }
+    }
+}
